4d2ce295f054a1ed001a047801992c7a4fea200a
PineappleANICODE <118062553+PineappleANICODE@users.noreply.github.com>
2023-01-21 11:33:25 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  var letter = document.getElementById("myInput").value;
+  var text;
+
+  
+  if (letter === "searcher") {
+    text = "Well done! Now for your reward:";
+    
+  
+  } else if (letter === "Searcher") {
+    text = "Well done! Now for your reward:";
+    
+    } else if (letter === "Nothing") {
+    text = "Clever, but wrong.";
+    
+  
+  } else {
+    text = "NO! HAVE YOU LEARNED NOTHING?";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
