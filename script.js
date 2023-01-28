a688037088d01e6c0e866f1ae1c4418ac9533cc1
PineappleANICODE <118062553+PineappleANICODE@users.noreply.github.com>
2023-01-28 10:21:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function myFunction() {
 
   
   if (letter === "searcher") {
-    text = "Well done! Now for your reward:";
+    text = "Well done! Now for your reward:     A single drop, a life can spiral out of control. His fear, your lies. Where did it go wrong? A narrative, a story, a peice of the puzzle. I know what you did even though you may not. ";
     
   
   } else if (letter === "Searcher") {
