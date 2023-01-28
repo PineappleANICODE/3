8e0b33eea26950e762fed3e4d15bb26d6ca1ecd0
PineappleANICODE <118062553+PineappleANICODE@users.noreply.github.com>
2023-01-28 10:42:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function myFunction() {
 
   
   if (letter === "searcher") {
-    text = "HAH, at least you can remember something.    A single drop, a life can spiral out of control. His fear, your lies. Where did it go wrong? A narrative, a story, a peice of the puzzle. I know what you did, even though you may not. ";
+    text = "HAH, at least you can remember something.    A single drop, a life can spiral out of control. His fear, your lies. Where did it go wrong? A narrative, a story, a peice of the puzzle. I know what you did, even though you may not. https://drive.google.com/file/d/1EEAIpmUCRle0-wnqXfLTvX8SKOPzWD8M/view?usp=sharing";
     
   
   } else if (letter === "Searcher") {
-    text = "Well done! Now for your reward:     ";
+    text = "HAH, at least you can remember something.    A single drop, a life can spiral out of control. His fear, your lies. Where did it go wrong? A narrative, a story, a peice of the puzzle. I know what you did, even though you may not. https://drive.google.com/file/d/1EEAIpmUCRle0-wnqXfLTvX8SKOPzWD8M/view?usp=sharing";
     
     } else if (letter === "Nothing") {
     text = "Clever, but wrong.";
